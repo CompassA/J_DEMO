786c6644d5e874059bdd12fe480b0ee3a2cc2144
fanqie <gyx645378029@163.com>
2019-11-24 13:28:00 +0800

    2 50

--- leetcode/src/main/java/Main.java
+++ leetcode/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author FanQie
@@ -26,4 +28,69 @@ public class Main {
         }
         return null;
     }
+
+    /**
+     * NO.2 Add Two Numbers
+     *
+     * You are given two non-empty linked lists representing two non-negative integers.
+     * The digits are stored in reverse order and each of their nodes contain a single digit.
+     * Add the two numbers and return it as a linked list.
+     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //设置头节点、前驱节点、进位数
+        final ListNode head = new ListNode(-1);
+        ListNode pre = head;
+        int carry = 0;
+
+        //只要有一个节点非空就进行循环
+        while (Objects.nonNull(l1) || Objects.nonNull(l2)) {
+            if (Objects.nonNull(l1)) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (Objects.nonNull(l2)) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            final ListNode cur = new ListNode(carry % 10);
+            carry /= 10;
+            pre.next = cur;
+            pre = cur;
+        }
+
+        //位数是否增加
+        if (carry != 0) {
+            pre.next = new ListNode(carry);
+        }
+
+        return head.next;
+    }
+
+    /**
+     * NO.50 Pow(x, n)
+     *
+     * Implement pow(x, n), which calculates x raised to the power n (x^n).
+     */
+    public double myPow(double x, int n) {
+        //对n进行检查
+        if (n == 0) {
+            return 1;
+        } else if (n < 0 && n != Integer.MIN_VALUE) {
+            return myPow(1 / x, -n);
+        }
+
+        //递归
+        final double t = myPow(x, n / 2);
+        if (n % 2 == 1) {
+            return t * t * x;
+        }
+        return t * t;
+    }
 }
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+ }
