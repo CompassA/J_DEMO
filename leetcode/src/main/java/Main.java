07cc1a0dc33f9eb2b8f660815dfad542fe386c2d
fanqie <gyx645378029@163.com>
2019-11-25 22:33:10 +0800

    双指针

--- leetcode/src/main/java/Main.java
+++ leetcode/src/main/java/Main.java
@@ -165,10 +165,74 @@ public class Main {
         }
         return toBeReplaced;
     }
+
+    /**
+     * NO.27 Remove Element
+     *
+     * Given an array nums and a value val,
+     * remove all instances of that value in-place and return the new length.
+     *
+     * Do not allocate extra space for another array,
+     * you must do this by modifying the input array in-place with O(1) extra memory.
+     *
+     * The order of elements can be changed.
+     * It doesn't matter what you leave beyond the new length.
+     */
+    public int removeElement(int[] nums, int val) {
+        int toBeReplaced = 0;
+        //扫描数组，把符合条件的数放到toBeReplaced上
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != val) {
+                if (toBeReplaced != i) {
+                    int tmp = nums[toBeReplaced];
+                    nums[toBeReplaced] = nums[i];
+                    nums[i] = tmp;
+                }
+                ++toBeReplaced;
+            }
+        }
+        return toBeReplaced;
+    }
+
+    /**
+     * NO. 80 Remove Duplicates from Sorted Array II
+     * Given a sorted array nums, remove the duplicates in-place such that
+     * duplicates appeared at most twice and return the new length.
+     *
+     * Do not allocate extra space for another array,
+     * you must do this by modifying the input array in-place with O(1) extra memory.
+     */
+    public int removeDuplicatesVersionTwo(int[] nums) {
+        int toBeReplaced = 2;
+        int curPos = 2;
+        while (curPos < nums.length) {
+            //只要当前数和倒数第二个数字不等就交换
+            if (nums[curPos] != nums[toBeReplaced - 2]) {
+                nums[curPos] = nums[toBeReplaced++];
+            }
+            ++curPos;
+        }
+        return toBeReplaced;
+//        int toBeReplaced = 0;
+//        int curPos = 0;
+//        while (curPos < nums.length) {
+//            //当前数置于toBeReplaced
+//            nums[toBeReplaced++] = nums[curPos++];
+//
+//            //处理第二位数相同的情况
+//            if (curPos < nums.length && nums[curPos] == nums[curPos-1]) {
+//                nums[toBeReplaced++] = nums[curPos++];
+//                while (curPos < nums.length && nums[curPos] == nums[curPos-1]) {
+//                    ++curPos;
+//                }
+//            }
+//        }
+//        return toBeReplaced;
+    }
 }
 
 class ListNode {
-      int val;
-      ListNode next;
+      public int val;
+      public ListNode next;
       ListNode(int x) { val = x; }
  }
