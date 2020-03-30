3922bfb757ff1c19ba4df8ef0cf1cce3fc4db8f7
番茄 <gyx645378029@163.com>
2020-03-30 15:55:00 +0800

    Spring Demo

---
+++ spring_demo/src/main/java/com/study/me/AppDemo.java
@@ -0,0 +1,16 @@
+package com.study.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author fanqie
+ * @date 2020/3/30
+ */
+@SpringBootApplication
+public class AppDemo {
+    public static void main( String[] args ) {
+        SpringApplication.run(AppDemo.class, args);
+    }
+}
