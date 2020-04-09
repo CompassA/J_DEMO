980cea6e404fbf8cbedc0a2714e724f373db82ba
番茄 <gyx645378029@163.com>
2020-04-09 17:56:10 +0800

    BinarySearch

--- leetcode/src/main/java/com/study/leetcode/binarysearch/BinarySearchProblem.java
+++ leetcode/src/main/java/com/study/leetcode/binarysearch/BinarySearchProblem.java
@@ -51,4 +51,76 @@ public class BinarySearchProblem {
         }
         return right;
     }
+
+    /**
+     * 4. Median of Two Sorted Arrays
+     * Hard
+     *
+     * 6319
+     *
+     * 966
+     *
+     * Add to List
+     *
+     * Share
+     * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+     *
+     * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+     *
+     * You may assume nums1 and nums2 cannot be both empty.
+     *
+     * Example 1:
+     *
+     * nums1 = [1, 3]
+     * nums2 = [2]
+     *
+     * The median is 2.0
+     * Example 2:
+     *
+     * nums1 = [1, 2]
+     * nums2 = [3, 4]
+     *
+     * The median is (2 + 3)/2 = 2.5
+     */
+    public double findMedianSortedArrays(int[] shorter, int[] longer) {
+        if (shorter.length > longer.length) {
+            return findMedianSortedArrays(longer, shorter);
+        }
+        int halfLength = (shorter.length + longer.length + 1) / 2;
+        //interval
+        int left = 0, right = shorter.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int otherMid = halfLength - mid;
+
+            //move shorter array interval
+            if (mid < shorter.length && shorter[mid] < longer[otherMid-1]) {
+                left = mid + 1;
+            } else if (mid > 0 && shorter[mid-1] > longer[otherMid]) {
+                right = mid - 1;
+            } else {
+                //find mid
+                int res;
+                if (mid == 0) {
+                    res = longer[halfLength-1];
+                } else if (otherMid == 0) {
+                    res = shorter[halfLength-1];
+                } else {
+                    res = Math.max(shorter[mid-1], longer[otherMid-1]);
+                }
+                if ((shorter.length + longer.length) % 2 == 1) {
+                    return res;
+                }
+                //find mid right
+                if (mid == shorter.length) {
+                    return (longer[otherMid]+ res) / 2.0;
+                } else if (otherMid == longer.length) {
+                    return (shorter[mid] + res) / 2.0;
+                } else {
+                    return (res + Math.min(shorter[mid], longer[otherMid])) / 2.0;
+                }
+            }
+        }
+        return 0;
+    }
 }
