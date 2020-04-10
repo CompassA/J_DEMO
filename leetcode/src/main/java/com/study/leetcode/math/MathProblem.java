9e94b720c2a3e31da26d794b1aa8adadfba744c1
番茄 <gyx645378029@163.com>
2020-04-10 21:23:59 +0800

    math、stack

--- leetcode/src/main/java/com/study/leetcode/math/MathProblem.java
+++ leetcode/src/main/java/com/study/leetcode/math/MathProblem.java
@@ -168,4 +168,50 @@ public class MathProblem {
         }
         return half == x || half / 10 == x;
     }
+
+    /**
+     * 41. First Missing Positive
+     * Hard
+     *
+     * 2869
+     *
+     * 737
+     *
+     * Add to List
+     *
+     * Share
+     * Given an unsorted integer array, find the smallest missing positive integer.
+     *
+     * Example 1:
+     *
+     * Input: [1,2,0]
+     * Output: 3
+     * Example 2:
+     *
+     * Input: [3,4,-1,1]
+     * Output: 2
+     * Example 3:
+     *
+     * Input: [7,8,9,11,12]
+     * Output: 1
+     * Note:
+     *
+     * Your algorithm should run in O(n) time and uses constant extra space.
+     */
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; ++i) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i]-1]) {
+                int index = nums[i] - 1;
+                int tmp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = tmp;
+            }
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
 }
--- leetcode/src/main/java/com/study/leetcode/stack/ValidParentheses.java
+++ leetcode/src/main/java/com/study/leetcode/stack/ValidParentheses.java
@@ -28,4 +28,55 @@ public class ValidParentheses {
 
         return stack.isEmpty();
     }
+
+
+    /**
+     * 32. Longest Valid Parentheses
+     * Hard
+     *
+     * 2965
+     *
+     * 126
+     *
+     * Add to List
+     *
+     * Share
+     * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+     *
+     * Example 1:
+     *
+     * Input: "(()"
+     * Output: 2
+     * Explanation: The longest valid parentheses substring is "()"
+     * Example 2:
+     *
+     * Input: ")()())"
+     * Output: 4
+     * Explanation: The longest valid parentheses substring is "()()"
+     */
+    public int longestValidParentheses(String s) {
+        Deque<Integer> stack = new ArrayDeque<>(s.length());
+        stack.offerLast(-1);
+        int res = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            switch (s.charAt(i)) {
+                case '(':
+                    stack.offerLast(i);
+                    break;
+                case ')':
+                    stack.pollLast();
+                    if (stack.isEmpty()) {
+                        stack.offerLast(i);
+                    } else {
+                        int len = i - stack.peekLast();
+                        if (res < len) {
+                            res = len;
+                        }
+                    }
+                    break;
+                default:
+            }
+        }
+        return res;
+    }
 }
