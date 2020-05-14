2716eb8a55a0d4562f930ce2827eb4d51665d8a7
番茄 <gyx645378029@163.com>
2020-05-14 17:50:45 +0800

    priority queue

---
+++ datastructure/src/main/java/org/study/data/MyPriorityQueue.java
@@ -0,0 +1,117 @@
+package org.study.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author fanqie
+ * @date 2020/5/14
+ */
+public class MyPriorityQueue<E extends Comparable<E>> {
+
+    private final MaxHeap<E> innerMaxHeap = new MaxHeap<>();
+
+    public void offer(final E elem) {
+        innerMaxHeap.add(elem);
+    }
+
+    public E poll() {
+        return innerMaxHeap.popMax();
+    }
+
+    public boolean isEmpty() {
+        return innerMaxHeap.size() == 0;
+    }
+
+    private static class MaxHeap<E extends Comparable<E>> {
+
+        private final List<E> list = new ArrayList<>();
+
+        public void add(final E elem) {
+            list.add(elem);
+            siftUp(list.size() - 1);
+        }
+
+        public E popMax() {
+            if (list.isEmpty()) {
+                return null;
+            }
+            final E maxElem = list.get(0);
+            final int endIndex = list.size() - 1;
+            swap(0, endIndex);
+            list.remove(endIndex);
+            siftDown(0);
+            return maxElem;
+        }
+
+        private void siftDown(int index) {
+            while (index < list.size()) {
+                final int maxIndex = getMaxIndex(index);
+                if (maxIndex == index) {
+                    return;
+                }
+                swap(index, maxIndex);
+                index = maxIndex;
+            }
+        }
+
+        private void siftUp(int index) {
+            while (index > 0) {
+                final int parentIndex = getParentIndex(index);
+                final E parent = list.get(parentIndex);
+                final E curElem = list.get(index);
+                if (parent.compareTo(curElem) >= 0) {
+                    return;
+                }
+                swap(parentIndex, index);
+                index = parentIndex;
+            }
+        }
+
+        private int getMaxIndex(final int index) {
+            int maxIndex = index;
+            E maxElem = list.get(index);
+            final int leftIndex = getLeftChildIndex(index);
+            if (leftIndex >= list.size()) {
+                return maxIndex;
+            }
+            if (list.get(leftIndex).compareTo(maxElem) > 0) {
+                maxIndex = leftIndex;
+                maxElem = list.get(leftIndex);
+            }
+
+            final int rightIndex = getRightChildIndex(index);
+            if (rightIndex >= list.size()) {
+                return maxIndex;
+            }
+            if (list.get(rightIndex).compareTo(maxElem) > 0) {
+                maxIndex = rightIndex;
+                maxElem = list.get(rightIndex);
+            }
+            return maxIndex;
+        }
+
+        private int getParentIndex(final int index) {
+            if (index <= 0) {
+                throw new RuntimeException("illegal index");
+            }
+            return (index - 1) / 2;
+        }
+
+        private int getLeftChildIndex(final int index) {
+            return index * 2 + 1;
+        }
+
+        private int getRightChildIndex(final int index) {
+            return index * 2 + 2;
+        }
+
+        private void swap(final int a, final int b) {
+            final E tmp = list.get(a);
+            list.set(a, list.get(b));
+            list.set(b, tmp);
+        }
+
+        public int size() { return list.size(); }
+    }
+}
---
+++ datastructure/src/test/java/org/study/data/MyPriorityQueueTest.java
@@ -0,0 +1,61 @@
+package org.study.data;
+
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * @author fanqie
+ * @date 2020/5/14
+ */
+public class MyPriorityQueueTest {
+
+    @Test
+    public void priorityQueueTest() {
+        final MyPriorityQueue<Integer> queue = new MyPriorityQueue<>();
+        for (int i = 0; i < 100; ++i) {
+            queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+
+    @Test
+    public void studentSortTest() {
+        final MyPriorityQueue<Person> queue = new MyPriorityQueue<>();
+        final Random random = new Random();
+        for (int i = 0; i < 100; ++i) {
+            queue.offer(new Person(UUID.randomUUID().toString(), random.nextInt(100)));
+        }
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+
+    private static class Person implements Comparable<Person> {
+        public String name;
+        public int age;
+
+        public Person(final String name, final int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public int compareTo(final Person o) {
+            return Integer.compare(this.age, o.age);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+}
