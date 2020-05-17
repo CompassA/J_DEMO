b2e6af630de3e04cb50c89e793f40eb1df7a5212
番茄 <gyx645378029@163.com>
2020-05-17 23:15:38 +0800

    20-05-17

--- leetcode/src/main/java/com/study/leetcode/mapandset/MapAndSet.java
+++ leetcode/src/main/java/com/study/leetcode/mapandset/MapAndSet.java
@@ -423,4 +423,90 @@ public class MapAndSet {
         }
     }
 
+    /**
+     * 1452. People Whose List of Favorite Companies Is Not a Subset of Another List
+     * Medium
+     *
+     * 40
+     *
+     * 57
+     *
+     * Add to List
+     *
+     * Share
+     * Given the array favoriteCompanies where favoriteCompanies[i] is the list of favorites companies for the ith person (indexed from 0).
+     *
+     * Return the indices of people whose list of favorite companies is not a subset of any other list of favorites companies. You must return the indices in increasing order.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: favoriteCompanies = [["leetcode","google","facebook"],["google","microsoft"],["google","facebook"],["google"],["amazon"]]
+     * Output: [0,1,4]
+     * Explanation:
+     * Person with index=2 has favoriteCompanies[2]=["google","facebook"] which is a subset of favoriteCompanies[0]=["leetcode","google","facebook"] corresponding to the person with index 0.
+     * Person with index=3 has favoriteCompanies[3]=["google"] which is a subset of favoriteCompanies[0]=["leetcode","google","facebook"] and favoriteCompanies[1]=["google","microsoft"].
+     * Other lists of favorite companies are not a subset of another list, therefore, the answer is [0,1,4].
+     * Example 2:
+     *
+     * Input: favoriteCompanies = [["leetcode","google","facebook"],["leetcode","amazon"],["facebook","google"]]
+     * Output: [0,1]
+     * Explanation: In this case favoriteCompanies[2]=["facebook","google"] is a subset of favoriteCompanies[0]=["leetcode","google","facebook"], therefore, the answer is [0,1].
+     * Example 3:
+     *
+     * Input: favoriteCompanies = [["leetcode"],["google"],["facebook"],["amazon"]]
+     * Output: [0,1,2,3]
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= favoriteCompanies.length <= 100
+     * 1 <= favoriteCompanies[i].length <= 500
+     * 1 <= favoriteCompanies[i][j].length <= 20
+     * All strings in favoriteCompanies[i] are distinct.
+     * All lists of favorite companies are distinct, that is, If we sort alphabetically each list then favoriteCompanies[i] != favoriteCompanies[j].
+     * All strings consist of lowercase English letters only.
+     */
+    public List<Integer> peopleIndexes(List<List<String>> favoriteCompanies) {
+        favoriteCompanies.forEach(Collections::sort);
+        List<Integer> res = new ArrayList<>();
+        int n = favoriteCompanies.size();
+        boolean[] covered = new boolean[n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j) {
+                    continue;
+                }
+                if (isCovered(favoriteCompanies.get(i), favoriteCompanies.get(j))) {
+                    covered[i] = true;
+                    break;
+                }
+            }
+            if (!covered[i]) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    private boolean isCovered(List<String> small, List<String> big) {
+        if (small.size() > big.size()) {
+            return false;
+        }
+        int smallIndex = 0;
+        int bigIndex = 0;
+        while (smallIndex < small.size() && bigIndex < big.size()) {
+            int cmpRes = small.get(smallIndex).compareTo(big.get(bigIndex));
+            if (cmpRes == 0) {
+                ++smallIndex;
+                ++bigIndex;
+            } else if (cmpRes > 0) {
+                ++bigIndex;
+            } else {
+                return false;
+            }
+        }
+        return smallIndex == small.size();
+    }
 }
