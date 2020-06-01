d6b4a5483d68c5eaba36e1c33361dc2bf67d5773
番茄 <gyx645378029@163.com>
2020-06-01 16:32:15 +0800

    adjust

---
+++ leetcode/src/main/java/com/study/leetcode/dfs/N_Queue.java
@@ -0,0 +1,158 @@
+package com.study.leetcode.dfs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author fanqie
+ * @date 2020/6/1
+ */
+public class N_Queue {
+
+    /**
+     * 51. N-Queens
+     * Hard
+     *
+     * 1706
+     *
+     * 69
+     *
+     * Add to List
+     *
+     * Share
+     * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+     *
+     *
+     *
+     * Given an integer n, return all distinct solutions to the n-queens puzzle.
+     *
+     * Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
+     *
+     * Example:
+     *
+     * Input: 4
+     * Output: [
+     *  [".Q..",  // Solution 1
+     *   "...Q",
+     *   "Q...",
+     *   "..Q."],
+     *
+     *  ["..Q.",  // Solution 2
+     *   "Q...",
+     *   "...Q",
+     *   ".Q.."]
+     * ]
+     * Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+     */
+    private static class Solution {
+        private List<List<String>> res = new ArrayList<>();
+
+        public List<List<String>> solveNQueens(int n) {
+            dfs(new int[n+1], new boolean[n+1], n, 1);
+            return res;
+        }
+
+        private void dfs(int[] board, boolean[] used, int n, int curX) {
+            if (curX == n+1) {
+                char[][] tmp = new char[n][n];
+                for (char[] line : tmp) {
+                    Arrays.fill(line, '.');
+                }
+                for (int x = 1; x <= n; ++x) {
+                    tmp[x-1][board[x]-1] = 'Q';
+                }
+                List<String> curRes = new ArrayList<>();
+                for (char[] line: tmp) {
+                    curRes.add(new String(line));
+                }
+                res.add(curRes);
+                return;
+            }
+            for (int y = 1; y <= n; ++y) {
+                if (used[y]) {
+                    continue;
+                }
+                boolean canGenerate = true;
+                for (int preX = 1; preX < curX; ++preX) {
+                    int preY = board[preX];
+                    if (Math.abs(preX - curX) == Math.abs(preY - y)) {
+                        canGenerate = false;
+                        break;
+                    }
+                }
+                if (canGenerate) {
+                    board[curX] = y;
+                    used[y] = true;
+                    dfs(board, used, n, curX + 1);
+                    used[y] = false;
+                }
+            }
+        }
+    }
+
+    /**
+     * 52. N-Queens II
+     * Hard
+     *
+     * 466
+     *
+     * 144
+     *
+     * Add to List
+     *
+     * Share
+     * The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+     *
+     *
+     *
+     * Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+     *
+     * Example:
+     *
+     * Input: 4
+     * Output: 2
+     * Explanation: There are two distinct solutions to the 4-queens puzzle as shown below.
+     * [
+     *  [".Q..",  // Solution 1
+     *   "...Q",
+     *   "Q...",
+     *   "..Q."],
+     *
+     *  ["..Q.",  // Solution 2
+     *   "Q...",
+     *   "...Q",
+     *   ".Q.."]
+     * ]
+     */
+    public int totalNQueens(int n) {
+        return dfs(new int[n+1], new boolean[n+1], n, 1);
+    }
+
+    private int dfs(int[] board, boolean[] used, int n, int curX) {
+        if (curX == n + 1) {
+            return 1;
+        }
+        int res = 0;
+        for (int y = 1; y <= n; ++y) {
+            if (used[y]) {
+                continue;
+            }
+            boolean canGenerate = true;
+            for (int preX = 1; preX < curX; ++preX) {
+                int preY = board[preX];
+                if (Math.abs(preX - curX) == Math.abs(preY - y)) {
+                    canGenerate = false;
+                    break;
+                }
+            }
+            if (canGenerate) {
+                board[curX] = y;
+                used[y] = true;
+                res += dfs(board, used, n, curX+1);
+                used[y] = false;
+            }
+        }
+        return res;
+    }
+}
