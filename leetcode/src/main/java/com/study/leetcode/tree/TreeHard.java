2cf71e0c837f7c83f291125531beb72856acf542
番茄 <gyx645378029@163.com>
2020-07-19 11:32:17 +0800

    fenwick tree problem

--- leetcode/src/main/java/com/study/leetcode/tree/TreeHard.java
+++ leetcode/src/main/java/com/study/leetcode/tree/TreeHard.java
@@ -50,4 +50,76 @@ public class TreeHard {
             return node;
         }
     }
+
+
+    /**
+     * 1505. Minimum Possible Integer After at Most K Adjacent Swaps On Digits
+     * Hard
+     *
+     * Given a string num representing the digits of a very large integer and an integer k.
+     *
+     * You are allowed to swap any two adjacent digits of the integer at most k times.
+     *
+     * Return the minimum integer you can obtain also as a string.
+     */
+    public String minInteger(String num, int k) {
+        Queue<Integer>[] numIndex = new Queue[10];
+        for (int i = 0; i < numIndex.length; ++i) {
+            numIndex[i] = new LinkedList<>();
+        }
+        for (int i = 0; i < num.length(); ++i) {
+            numIndex[num.charAt(i)-'0'].offer(i);
+        }
+        FenwickTree tree = new FenwickTree(num.length());
+        boolean[] moved = new boolean[num.length()];
+        StringBuilder res = new StringBuilder();
+        while (k > 0 && res.length() < num.length()) {
+            for (int i = 0; i < 10; ++i) {
+                if (numIndex[i].isEmpty()) {
+                    continue;
+                }
+                int frontIndex = numIndex[i].peek();
+                int cost = frontIndex - tree.query(frontIndex - 1);
+                if (cost > k) {
+                    continue;
+                }
+                k -= cost;
+                res.append(i);
+                tree.update(frontIndex, 1);
+                moved[frontIndex] = true;
+                numIndex[i].poll();
+                break;
+            }
+        }
+        for (int i = 0; i < moved.length; ++i) {
+            if (!moved[i]) {
+                res.append(num.charAt(i));
+            }
+        }
+        return res.toString();
+    }
+    private static class FenwickTree {
+        private final int[] nums;
+        public FenwickTree(int n) {
+            this.nums = new int[n + 1];
+        }
+
+        public int query(int index) {
+            index++;
+            int res = 0;
+            while (index > 0) {
+                res += nums[index];
+                index -= (index & -index);
+            }
+            return res;
+        }
+
+        public void update(int index, int val) {
+            index++;
+            while (index < nums.length) {
+                nums[index] += val;
+                index += (index & -index);
+            }
+        }
+    }
 }
