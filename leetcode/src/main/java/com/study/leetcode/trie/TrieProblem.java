775da7968fc23c922030f3c2c8c51ba2f38de258
Tomato <gyx645378029@163.com>
2021-01-02 23:02:21 +0800

    21-01-02

--- leetcode/src/main/java/com/study/leetcode/trie/TrieProblem.java
+++ leetcode/src/main/java/com/study/leetcode/trie/TrieProblem.java
@@ -1,5 +1,6 @@
 package com.study.leetcode.trie;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -55,6 +56,70 @@ public class TrieProblem {
                 }
             }
             return sum;
+        }
+    }
+
+    //=======================Hard=====================================
+
+    /**
+     * 1707. Maximum XOR With an Element From Array
+     * https://leetcode.com/problems/maximum-xor-with-an-element-from-array/
+     */
+    public int[] maximizeXor(int[] nums, int[][] queries) {
+        int[][] queryInfo = new int[queries.length][];
+        for (int i = 0; i < queries.length; ++i) {
+            queryInfo[i] = new int[3];
+            queryInfo[i][0] = queries[i][0];
+            queryInfo[i][1] = queries[i][1];
+            queryInfo[i][2] = i;
+        }
+        Arrays.sort(nums);
+        Arrays.sort(queryInfo, (query1, query2) -> query1[1] - query2[1]);
+
+        int[] res = new int[queries.length];
+        Trie root = new Trie();
+        int numIndex = 0;
+        for (int[] query : queryInfo) {
+            while (numIndex < nums.length && nums[numIndex] <= query[1]) {
+                root.insert(nums[numIndex]);
+                ++numIndex;
+            }
+            res[query[2]] = root.query(query[0]);
+        }
+        return res;
+    }
+
+    private static class TrieII {
+        TrieII[] children = new TrieII[2];
+
+        public void insert(int num) {
+            TrieII curNode = this;
+            for (int i = 31; i >= 0; --i) {
+                int bit = (num >> i) & 1;
+                if (curNode.children[bit] == null) {
+                    curNode.children[bit] = new TrieII();
+                }
+                curNode = curNode.children[bit];
+            }
+        }
+
+        public int query(int num) {
+            int res = 0;
+            TrieII curNode = this;
+            for (int i = 31; i >= 0; --i) {
+                if (curNode.children[0] == null && curNode.children[1] == null) {
+                    return -1;
+                }
+                int bit = (num >> i) & 1;
+                TrieII next = curNode.children[1 - bit];
+                if (next != null) {
+                    res |= (1 << i);
+                    curNode = next;
+                } else {
+                    curNode = curNode.children[bit];
+                }
+            }
+            return res;
         }
     }
 }
